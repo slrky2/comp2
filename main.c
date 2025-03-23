feac0ad9fe0aa4f2835b31ded09cdc8e04f8f3c5
slrky2 <144831052+slrky2@users.noreply.github.com>
2025-03-23 12:42:12 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ struct node {
 struct Queue {
     Node* head;
     Node* tail;
+    int capacity;
+    int size;
 };
 typedef struct Queue Queue;
 enum status {
@@ -15,7 +17,7 @@ enum status {
 };
 typedef enum status Status;
 
-Queue* createQueue(int val);
+Queue* createQueue(int cap);
 
 Status insert(Queue*, int);
 
@@ -27,6 +29,10 @@ int is_empty(Queue*);
 
 Status destroy_queue(Queue*);
 
+void clear_key_buffer(void);
+
+int ferry(int, int, char);
+
 int main(int argc, char * argv[]) {
     /*
      *first line the the # of inputs
@@ -35,58 +41,129 @@ int main(int argc, char * argv[]) {
      *m = amount of cars
      *lines following lenght of cars in cm and whether they are on the right or left
      */
-    Queue * q = createQueue(1);
-    insert(q, 2);
-    insert(q, 3);
-    printf("%d\n", front(q));
-    service(q);
-    printf("%d\n", front(q));
-    service(q);
-    printf("%d\n", front(q));
-    service(q);
-    printf("%d\n", is_empty(q));
+    //todo make sure to update size and capacity in every function new variables in the queue struct
+    //todo it may be easier to keep the sizes as local in main in that case we dont need size and capacity in the struct
+    int num;
+    int cars;
+    int ferry_length;
+    int car_length;
+    char direction;
+    Queue* left = NULL;
+    Queue* right = NULL;
+
+    scanf("%d", &num);
+    //just getting info to begin the evaulation
+    for (int i = 0; i < num; i++) {
+        printf("%d\n", num);
+        clear_key_buffer();
+        scanf("%d", &ferry_length);
+        scanf("%d", &cars);
+        clear_key_buffer();
+        printf("%d\n", ferry_length);
+        printf("%d\n", cars);
+
+        //initialize the queues
+        left = createQueue(cars);
+        right = createQueue(cars);
+    }
+        //this is setting up the queues
+        // todo make sure that the case given in the pdf actaully works currently doesnt run
+        for (int j = 0; j < cars; j++) {
+            scanf("%d", &car_length);
+            printf("lenght of the car %d\n", car_length);
+            scanf(" %c", &direction);
+            printf("direction = %c\n", direction);
+            clear_key_buffer(); // we only need to know the first letter of the direction
+            //todo create the queues
+            switch (direction) {
+                case 'r':
+                    insert(right, car_length);
+                break;
+                case 'l':
+                    insert(left, car_length);
+                break;
+                default: printf("couldnt evaulate direction\n");
+            }
+
+// todo do the work pop off the respective queue if the car can fit  on the ferry
+
+            // while (ferry_length >= car_length/100) { // because m/100 = cm
+            //     switch (direction) {
+            //         case 'r':
+            //             service(right);
+            //         break;
+            //         case 'l':
+            //             service(left);
+            //         break;
+            //         default: printf("couldnt evaulate direction while servicing cars\n");
+            //     }
+            // }
+
+            // do work or pop the stacks, the stacks will always become empty this an be a function
+        }
+    while (!is_empty(left)) {
+        printf("left %d \t", front(left));
+        service(left);
+    }
+    while (!is_empty(right)) {
+        printf("right %d \t", front(right));
+        service(right);
+    }
 
 
-
-    destroy_queue(q); // this only destroys the queue struct make sure that all nodes are freed before this call
+    destroy_queue(left); // this only destroys the queue struct make sure that all nodes are freed before this call
+    destroy_queue(right);
     return 0;
 }
-Queue* createQueue(int val) {
+
+// cm = m/100
+// the function should start with the direction that was stated first
+
+ int ferry(int ferry_lengthM, int car_lengthCM, char direction) {
+   }
+
+
+
+
+
+Queue* createQueue(int cap) {
     Queue* q = (Queue*)malloc(sizeof(Queue));
     if (q!=NULL) {
         q->head = NULL;
-        insert(q, val);
-            //the length is one when created
-            q -> tail = q-> head;
+        q->tail = NULL;
+        q->size = 0;
+        q -> capacity = cap;
+        return q;
+    }
+}
+
+    Status insert(Queue* q, int data) {
+        if (q==NULL) {
+            return FAILURE;
         }
-    return q;
+        Node* new_node = (Node*)malloc(sizeof(Node));
+        if (new_node!= NULL) {
+            new_node -> data = data;
+            new_node -> next = NULL;
+            if (q->head == NULL) {
+                q->head = new_node;
+                q -> tail = new_node;
+                printf("inserted node %d at %p\n", new_node->data, new_node);
+
+            } else {
+                while (q -> tail -> next != NULL) {
+                    q -> tail = q -> tail -> next;
+                }
+                q -> tail -> next = new_node;
+                q -> tail = new_node;
+                printf("inserted node %d at %p\n", new_node->data, new_node);
+
+            }
+        }
+        q->size++;
+        return SUCCESS;
     }
 
-Status insert(Queue* q, int data) {
-    if (q==NULL) {
-        return FAILURE;
-    }
-    Node* new_node = (Node*)malloc(sizeof(Node));
-    if (new_node!= NULL) {
-        new_node -> data = data;
-        new_node -> next = NULL;
-        if (q->head == NULL) {
-            q->head = new_node;
-            q -> tail = new_node;
-            printf("inserted node %d at %p\n", new_node->data, new_node);
-            return SUCCESS;
-        } else {
-            while (q -> tail -> next != NULL) {
-                q -> tail = q -> tail -> next;
-            }
-            q -> tail -> next = new_node;
-            q -> tail = new_node;
-            printf("inserted node %d at %p\n", new_node->data, new_node);
-            return SUCCESS;
-        }
-    }
-    return FAILURE;
-}
 
 Status service(Queue* q) {
     if (q->head!=NULL) {
@@ -94,6 +171,7 @@ Status service(Queue* q) {
         q->head = q -> head -> next;
         printf("servicing %d\n", delete_me->data);
         free(delete_me);
+        q->size--;
         return SUCCESS;
     }
     return FAILURE;
@@ -119,4 +197,12 @@ Status destroy_queue(Queue* q) {
     }
     printf("unaccessible queue\n");
     return FAILURE;
+}
+
+void clear_key_buffer(void) {
+    char c;
+    scanf("%c", &c);
+    while (c != '\n') {
+        scanf("%c", &c);
+    }
 }
